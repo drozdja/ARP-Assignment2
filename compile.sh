16594272dc400b57e46bc92b52679241a0ce6ffe
Tomasz <tomek2.strzesak@gmail.com>
2023-01-18 18:33:29 +0100

    Add new code to compile.sh

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
+mkdir bin
 gcc src/master.c -Iinclude -lncurses -lbmp -lrt -lm -lpthread -o bin/master
 gcc src/processA.c -Iinclude -lncurses -lbmp -lrt -lpthread -o bin/processA
 gcc src/processB.c -Iinclude -lncurses -lbmp -lrt -lpthread -o bin/processB
